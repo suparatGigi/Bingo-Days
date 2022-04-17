e58a6b42a1034018d319ee95f47680ed6379bfea
ArtarMatcee <6434476923@student.chula.ac.th>
2022-04-17 20:10:31 +0700

    Artar Update

--- src/Main/BingoGame.java
+++ src/Main/BingoGame.java
@@ -8,13 +8,19 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 
 public class BingoGame {
+    private BingoNumbers bingoNumbers;
+    private BingoGrid dummyGrid;
+    private BingoGridDays daysGrid;
+
     private MouseClickListener mouseListener;
     private ActionListener timer;
     private ActionListener buttonListener;
     private final int DELAY;
+    private boolean startGame = false;
+    private boolean winner = false;
 
 
-    public BingoGame(GameManager gm){
+    public BingoGame(GameManager gm) {
         createBingoButton();
 
         MouseClickListener mouseListener = new MouseClickListener();
@@ -23,7 +29,7 @@ public class BingoGame {
         Timer t = new Timer(DELAY, timer);
         t.start();
 
-        add(daysGrid);
+        /*add(daysGrid);
         setVisible(true);
 
         add(bingoNumbers);
@@ -31,6 +37,8 @@ public class BingoGame {
 
         daysGrid.addMouseListener(mouseListener);
         setVisible(true);
+
+         */
     }
 
     public void createBingoButton() {
@@ -44,12 +52,39 @@ public class BingoGame {
         ImageIcon buttonIcon = new ImageIcon(getClass().getClassLoader().getResource("image/bingoButton.png")); // เพิ่มรูปปุ่ม
         bingoButton.setIcon(buttonIcon);
 
-        ActionHandler aHandler = new ActionHandler();
-        bingoButton.addActionListener(aHandler);
+        buttonListener = new ButtonListener();
+        bingoButton.addActionListener(buttonListener);
+    }
+
+    class Timer implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            Object source = event.getSource();
+            if (startGame && !winner) {
+                bingoNumbers.generateNumber();
+                daysGrid.isCalled();
+                daysGrid.setWinnerMessage("");
+                bingoNumbers.repaint();
+            }
+        }
+    }
+
+    class ButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (source == bingo) { //checks if the human grid has won
+                if (!winner) {
+                    if (daysGrid.checkWin()) {
+                        daysGrid.setWinnerMessage("WINNER: HUMAN");
+                        winner = true;
+                    } else {
+                        daysGrid.setWinnerMessage("Sorry, you haven't gotten bingo.");
+                    }
+                }
+        }
     }
 
 
 
 
 
-\
+
